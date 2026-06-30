4f6620e65892755a56c705a4f64c3346bcdb4ba9
Ivan Yourshaw <39739503+iyourshaw@users.noreply.github.com>
2026-06-30 00:35:27 -0600

    log level

--- jpo-deduplicator/src/test/java/us/dot/its/jpo/deduplicator/filters/bsm/whitelist/BsmWhitelistPropertiesTest.java
+++ jpo-deduplicator/src/test/java/us/dot/its/jpo/deduplicator/filters/bsm/whitelist/BsmWhitelistPropertiesTest.java
@@ -55,7 +55,7 @@ public class BsmWhitelistPropertiesTest {
     private static void addParams(ArrayList<Object[]> params, String hexId, boolean expectWhitelisted) {
         var tempId = new TemporaryID();
         tempId.setValue(hexId);
-        log.info("tempId: {}, value: {}, num: {}", tempId.toString(), tempId.getValue(), tempId.getOctets());
+        log.debug("tempId: {}, value: {}, num: {}", tempId, tempId.getValue(), tempId.getOctets());
         params.add(new Object[] { tempId, expectWhitelisted} );
     }
 }
